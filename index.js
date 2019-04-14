b1044a5e59f4dfbf4e85fe0356dc18eb4a77ba14
rizalfsd <48785596+rizalfsd@users.noreply.github.com>
2019-04-14 22:55:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'Happy Birth Day','Nur Fadhilah','Love You' ],
+			strings: [ 'Happy Birth Day','Nur Fadhilah','I Love You','❤❤❤' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
